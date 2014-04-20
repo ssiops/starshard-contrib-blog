6400559de3204a6834f9229634b84e4bed7debe7
akasfei <akasfei@gmail.com>
2014-04-20 15:53:04 +0800

    $alertService

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,36 @@
-angular.module('blog', ['ui.bootstrap']);
+angular.module('blog', ['ui.bootstrap']).
+  factory('$alertService', ['$rootScope', function ($rootScope) {
+    return {
+      show: function () {
+        return $rootScope.$broadcast('alertEvent', {show: true});
+      },
+      hide: function () {
+        return $rootScope.$broadcast('alertEvent', {show: false});
+      },
+      send: function (opt) {
+        if (typeof opt === 'string')
+          return $rootScope.$broadcast('alertEvent', {show: true, msg: opt});
+        else {
+          var e = {show: true, style: 'warning'};
+          if (typeof opt.style !== undefined)
+            e.style = opt.style;
+          if (typeof opt.msg === 'undefined')
+            return;
+          e.msg = opt.msg;
+          return $rootScope.$broadcast('alertEvent', e);
+        }
+      }
+    }
+  }]);
+
+function floatMsgCtrl ($scope) {
+  $scope.show = false;
+  $scope.style = 'warning';
+  $scope.$on('alertEvent', function (e, opt) {
+    for (var prop in opt)
+      $scope[prop] = opt[prop];
+  });
+}
 
 !function () {
   
